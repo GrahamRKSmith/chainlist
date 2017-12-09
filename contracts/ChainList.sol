ff8d326ec63f3d3778975b21f3e1e2684e8b4e4b
Zarak Mahmud <zarak.mahmud@utoronto.ca>
2017-12-09 15:38:25 +0500

    Clean install for 06 - add code to frontend for events

---
+++ contracts/ChainList.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.11;
+
+contract ChainList {
+    // State variables
+    address seller;
+    string name;
+    string description;
+    uint256 price;
+
+    // Events
+    event sellArticleEvent (
+          address indexed _seller,
+          string _name,
+          uint256 _price
+    );
+
+    // sell an article
+    function sellArticle(string _name, string _description, uint256 _price) public {
+        seller = msg.sender;
+        name = _name;
+        description = _description;
+        price = _price;
+        sellArticleEvent(seller, name, price);
+    }
+
+    // get the article
+    function getArticle() public constant returns (
+        address _seller,
+        string _name,
+        string _description,
+        uint256 _price) {
+        return(seller, name, description, price);
+    }
+}
